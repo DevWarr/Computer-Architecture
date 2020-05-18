47166cd0103d1ba1e22eeb66527b0678c555fe17
Devin Warrick <dev.warr@gmail.com>
2020-05-18 16:32:13 -0500

    Implements the most basic, hard-coded CPU

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,21 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.ram = [0b0] * 0x100
+        self.reg = [0] * 0x8
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +27,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,14 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        ir = 0
+        while ir != 0x01:
+            ir = self.ram_read(self.pc)
+            
+            if ir == 0x82:
+                self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2)
+                self.pc += 1 + 2
+            elif ir == 0x47:
+                print(self.reg[self.ram_read(self.pc + 1)])
+                self.pc += 1 + 1
+
