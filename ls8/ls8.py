f3091e4ccd9e45453ce28b545297fa3d7a2b1f66
Devin Warrick <dev.warr@gmail.com>
2020-05-18 17:10:42 -0500

    Adds check to ensure example is given to the CPU

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,17 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+def run_cpu(file):
+    cpu = CPU()
 
-cpu.load()
-cpu.run()
+    cpu.load(file)
+    cpu.run()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please pick an example program to execute like so:")
+        print("py ls8.py examples/print8.ls8")
+    elif sys.argv[1][-4:] != ".ls8":
+        print("Please pick a program ending in '.ls8'")
+    else:
+        run_cpu(sys.argv[1])
